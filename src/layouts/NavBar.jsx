cbd62e43a30da7dfcd5cb2637768a5409aa51fbb
Kolawole Akintayo <kolawoleakintayok@gmail.com>
2025-07-29 12:23:06 +0100

    update navbar

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../assets/images/logo2.png";
 import { Link, useNavigate } from "react-router-dom";
+import { navItems } from "../utils/constants";
 
 const NavBar = () => {
 
@@ -10,13 +11,22 @@ const NavBar = () => {
       <Link to={"/"}>
         <div className="flex items-center gap-2">
           <img src={logo} className="w-[150px] -ml-10" alt="Logo" />
-          <div className="flex flex-col text-white -ml-5 my-auto">
-            <h1 className="text-secondary text-lg md:text-2xl font-semibold">
-              SAFETY SECURITY,INC.
-            </h1>
-            <p className="text-secondary text-xs md:text-sm font-normal">
-              ....protecting your businesses and properties
-            </p>
+
+          <div className="justify-between flex w-full">
+            <div className="flex flex-col text-white -ml-5 my-auto">
+              <h1 className="text-secondary text-lg md:text-2xl font-semibold">
+                SAFETY SECURITY,INC.
+              </h1>
+              <p className="text-secondary text-xs md:text-sm font-normal">
+                securing your businesses and properties
+              </p>
+            </div>
+            {/* <div>
+              {navItems.map(navItem) }
+            </div>
+            <div>
+              <button type="button" className=" bg-white text-blue-800 p-3 font-semibold text-sm rounded-full">Request A Quote</button>
+            </div> */}
           </div>
         </div>
       </Link>
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const navItems = [
+    { label: 'Home', link: '/' },
+    { label: 'About Us', link: '/about' },
+    { label: 'Our Services', link: '/services' },
+    { label: 'Contact Us', link: '/contacts' },
+];
